af68cb82bb0a79f3c1b3735dcce615f6a500015e
Elen Tumasyan <Elen Tumasyan>
2016-11-14 19:26:21 +0400

    Watermelon update

--- Watermelon.java
+++ Watermelon.java
@@ -2,17 +2,14 @@ import java.util.Scanner;
 
 
 public class Watermelon {
-
 	public static void main(String[] args) {
 
-
 		Scanner in = new Scanner(System.in);
-		int w = in.nextShort();
-		if(w%2 == 0 && w > 2)
+		int weight = in.nextShort();
+		
+		if(weight % 2 == 0 && weight > 2)
 			System.out.print("YES");
 		else
 			System.out.print("NO");
-		
 	}
-
 }
